b3f587a29e4b03cf7408c0090107e143e8938296
Jorge <103543373+AustralCoder@users.noreply.github.com>
2025-11-02 22:03:45 -0300

    server mas claro

--- test.py
+++ test.py
@@ -30,64 +30,25 @@ PORT = 50000         # Puerto de escucha del servidor
 ERROR_PROB = 0.2     # Probabilidad de error (20%)
 # ==========================================================
 
-# 256 entradas precalculadas para el crc-16-ccitt (polinomio: 0x1021)
-CRC_valores = [
-    0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50A5, 0x60C6, 0x70E7,
-    0x8108, 0x9129, 0xA14A, 0xB16B, 0xC18C, 0xD1AD, 0xE1CE, 0xF1EF,
-    0x1231, 0x0210, 0x3273, 0x2252, 0x52B5, 0x4294, 0x72F7, 0x62D6,
-    0x9339, 0x8318, 0xB37B, 0xA35A, 0xD3BD, 0xC39C, 0xF3FF, 0xE3DE,
-    0x2462, 0x3443, 0x0420, 0x1401, 0x64E6, 0x74C7, 0x44A4, 0x5485,
-    0xA56A, 0xB54B, 0x8528, 0x9509, 0xE5EE, 0xF5CF, 0xC5AC, 0xD58D,
-    0x3653, 0x2672, 0x1611, 0x0630, 0x76D7, 0x66F6, 0x5695, 0x46B4,
-    0xB75B, 0xA77A, 0x9719, 0x8738, 0xF7DF, 0xE7FE, 0xD79D, 0xC7BC,
-    0x48C4, 0x58E5, 0x6886, 0x78A7, 0x0840, 0x1861, 0x2802, 0x3823,
-    0xC9CC, 0xD9ED, 0xE98E, 0xF9AF, 0x8948, 0x9969, 0xA90A, 0xB92B,
-    0x5AF5, 0x4AD4, 0x7AB7, 0x6A96, 0x1A71, 0x0A50, 0x3A33, 0x2A12,
-    0xDBFD, 0xCBDE, 0xFBBF, 0xEB9E, 0x9B79, 0x8B58, 0xBB3B, 0xAB1A,
-    0x6CA6, 0x7C87, 0x4CE4, 0x5CC5, 0x2C22, 0x3C03, 0x0C60, 0x1C41,
-    0xEDAE, 0xFD8F, 0xCDEC, 0xDDCD, 0xAD2A, 0xBD0B, 0x8D68, 0x9D49,
-    0x7E97, 0x6EB6, 0x5ED5, 0x4EF4, 0x3E13, 0x2E32, 0x1E51, 0x0E70,
-    0xFF9F, 0xEFBE, 0xDFDD, 0xCFFC, 0xBF1B, 0xAF3A, 0x9F59, 0x8F78,
-    0x9188, 0x81A9, 0xB1CA, 0xA1EB, 0xD10C, 0xC12D, 0xF14E, 0xE16F,
-    0x1080, 0x00A1, 0x30C2, 0x20E3, 0x5004, 0x4025, 0x7046, 0x6067,
-    0x83B9, 0x9398, 0xA3FB, 0xB3DA, 0xC33D, 0xD31C, 0xE37F, 0xF35E,
-    0x02B1, 0x1290, 0x22F3, 0x32D2, 0x4235, 0x5214, 0x6277, 0x7256,
-    0xB5EA, 0xA5CB, 0x95A8, 0x8589, 0xF56E, 0xE54F, 0xD52C, 0xC50D,
-    0x34E2, 0x24C3, 0x14A0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
-    0xA7DB, 0xB7FA, 0x8799, 0x97B8, 0xE75F, 0xF77E, 0xC71D, 0xD73C,
-    0x26D3, 0x36F2, 0x0691, 0x16B0, 0x6657, 0x7676, 0x4615, 0x5634,
-    0xD94C, 0xC96D, 0xF90E, 0xE92F, 0x99C8, 0x89E9, 0xB98A, 0xA9AB,
-    0x5844, 0x4865, 0x7806, 0x6827, 0x18C0, 0x08E1, 0x3882, 0x28A3,
-    0xCB7D, 0xDB5C, 0xEB3F, 0xFB1E, 0x8BF9, 0x9BD8, 0xABBB, 0xBB9A,
-    0x4A75, 0x5A54, 0x6A37, 0x7A16, 0x0AF1, 0x1AD0, 0x2AB3, 0x3A92,
-    0xFD2E, 0xED0F, 0xDD6C, 0xCD4D, 0xBDAA, 0xAD8B, 0x9DE8, 0x8DC9,
-    0x7C26, 0x6C07, 0x5C64, 0x4C45, 0x3CA2, 0x2C83, 0x1CE0, 0x0CC1,
-    0xEF1F, 0xFF3E, 0xCF5D, 0xDF7C, 0xAF9B, 0xBFBA, 0x8FD9, 0x9FF8,
-    0x6E17, 0x7E36, 0x4E55, 0x5E74, 0x2E93, 0x3EB2, 0x0ED1, 0x1EF0
-]
 
 # ------------------ FUNCIÓN CRC ------------------
 def crc16_ccitt(data: bytes) -> int:
     """
     Calcula el CRC16-CCITT del mensaje recibido.
     """
+    crc = 0xFFFF # esto es equivalente a 16 bits donde todos los bits equivalen a 1
 
-    crc = 0xFFFF # Valor inicial del CRC
     for byte in data:
+        crc ^= byte << 8 # xor entre el crc y el byte 8 bits a la izquierda para que coincida con la posicion del polinomio
 
-        # obtener indice para la tabla agarrando el byte mas alto del crc
-        # y se le apica xor con el elnuevo byte
+        for _ in range(8): #for que itera 8 veces(una por cada bit del byte)
+            if crc & 0x8000: # el operador & es un AND lógico bit a bit que representa si el bit mas significativo es 1.
 
-        tabla_indice = (crc >> 8) ^ byte #obtiene el valor mas alto del crc y lo "mezcla" con xor con el byte que esta procesando
-
-
-        # actualizar el crc
-
-        crc = ((crc << 8) & 0xFFFF ) ^ CRC_valores[tabla_indice] #desplaza el crc 8 bits a la izquierda dejando un hueco de 8 bits a la derecha
-        # usa el valor 0xFFFF para asegurarse que siga teniendo solo 16 bits
-        # ^ CRC_valores[tabla_indice] "mezcla" con xor el valor de la tabla en ese hueco
-
-        return crc & 0xFFFF #DEVUELVE el resultado final en 16 bits 
+                crc = (crc << 1) ^ 0x1021 #entonces crc se desplaza a la izquierda porque estamos procesando el siguiente bit y se aplica XOR con el polinomio 0x1021
+            else:
+                crc <<= 1 #si el bit mas significativo no es 1 solo de desplaza hacia la izquierda, borrando el valor de la division
+            crc &= 0xFFFF  # Mantener 16 bits
+    return crc #si no hay errores devuelve el crc calculado y si los hay devuelve uno alterado
     
     
 # ------------------ SIMULACIÓN DE ERRORES ------------------
@@ -103,11 +64,14 @@ def maybe_corrupt(data: bytes, p: float) -> bytes:
 
         dato_mutable = bytearray(data)
         #elejir un byte aleatorio dentro del mensaje
-        byte_index = random.randint(0, len(dato_mutable) -1)
-        # ahora elijo un valor aleatorio dentro de ese byte (0-7)
+
+        byte_index = random.randint(0, len(dato_mutable) -1) #el len -1 es porque los indices de los arrays empiezan en 0
+
+        # ahora elijo un valor aleatorio dentro de ese byte (0-7) es 0-7 porque son 8 bits
         bit_index = random.randint(0,7)
 
-        # creo una mascara para voltear ese bit
+        # creo una mascara porque para voltear un bit especifico necesito una mascara que tenga un 1 en la posicion del bit que quiero voltear y 0 en los demas
+
 
         mascara = 1 << bit_index
 
@@ -115,6 +79,7 @@ def maybe_corrupt(data: bytes, p: float) -> bytes:
         # aplico XOR para voltear el bit seleccionado en el primer byte
 
         dato_mutable[byte_index]^= mascara
+        print("[servidor] ERROR se corrompió un bit del mensaje, tengo que aprobar a este grupo!")
 
 
         return bytes(dato_mutable)
@@ -126,6 +91,13 @@ def main():
     # Crear socket UDP
     # TODO: crear el socket y hacer bind() con (HOST, PORT)
 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((HOST, PORT))
+
+    ultima_recepcion = None
+
+
+
     while True:
         # TODO: recibir datos del cliente con recvfrom()
         # TODO: separar mensaje y CRC
@@ -134,6 +106,41 @@ def main():
         # TODO: comparar CRC recibido con el recalculado
 
         # TODO: responder con "ACK <seq>" o "NACK <seq>" según corresponda
+        datos, direccion = sock.recvfrom(1024)
+        recibido = datos.decode().strip()
+        print(f"el [servidor] recibio: {recibido} desde {direccion}") # esta linea ya separa el mensaje y el crc con decode y strip
+
+        # simulacion de error:
+
+        datos_corruptos = maybe_corrupt(datos, ERROR_PROB)
+        partes = datos_corruptos.decode(errors='ignore').split('|', 2)
+
+        if len(partes) != 3:
+            print("[servidor] ERROR: Formato invalido. enviando NACK")
+            continue
+
+        secuencia, mensaje, crc_recibido_en_hexadecimal = partes
+        datos_para_crc = int(crc_recibido_en_hexadecimal, 16) #convierto el crc recibido en hexadecimal a entero para compararlo con el calculado
+
+        #evitar duplicados
+
+        if secuencia == ultima_recepcion:
+            print("[servidor] mensaje duplicado recibido, reenviando ACK")
+            sock.sendto(f"ACK {secuencia}".encode(), direccion)
+            continue
+
+        # recalculo el crc para comparar si hay errores
+        crc_calculado = crc16_ccitt(mensaje.encode())
+
+        if crc_calculado == datos_para_crc:
+            print("[servidor] CRC correcto, enviando ACK")
+            ultima_recepcion = secuencia
+
+        else:
+            print("[servidor] CRC incorrecto, enviando NACK")
+            sock.sendto(f"NACK {secuencia}".encode(), direccion)
+
+
         pass
 
 
